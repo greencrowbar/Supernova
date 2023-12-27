891d8faa2957c83932fb4d9fd5a5eb817fd31fdc
haster <andrew@nxweb.com>
2023-12-27 17:00:53 -0500

    update to latest frontend

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 2001;
+if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
   const address = server.address();
